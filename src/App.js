654beeca2dfcc989ece3959391e4136de57be48f
udayrajmandke <urmandke@gmail.com>
2024-05-10 17:12:27 -0700

    chore: Setup prettier.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import './App.css';
-import Home from './components/home/Home';
+import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+      <Home />
     </div>
   );
 }
